fbe56a2c2d0c9f40da565bf3a4337da9cf71d365
yanhai <shimmly@live.cn>
2017-06-21 14:21:30 +0800

    init: from create-react-app

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+  },
+  extends: 'airbnb',
+  plugins: [
+    'react',
+    'jsx-a11y',
+    'import'
+  ],
+  rules: {
+    'react/jsx-filename-extension': [1, {
+      'extensions': ['.js', '.jsx']
+    }],
+    'react/prefer-stateless-function': [1]
+  }
+};
